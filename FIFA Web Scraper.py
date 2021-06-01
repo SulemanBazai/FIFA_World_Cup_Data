507cf8e47e8410589c5e450cfd70cd2fcfcb4dfd
sulemanbazai <sulemanbazai@gmail.com>
2021-06-01 02:12:25 -0500

    Update with building dictionary and Pandas DF for plotting and have two potential choices for animated plots.

--- FIFA Web Scraper.py
+++ FIFA Web Scraper.py
@@ -5,6 +5,8 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 import re
+import pandas as pd
+import copy
 
 
 # Set URL, open and read the html page, and create the BeautifulSoup object.
@@ -77,3 +79,59 @@ for i in range(len(score_list)):
     grouped_match_list.append([first_team_list[i], score_list[i], second_team_list[i]])
 
 print(grouped_match_list)
+####CODE FROM SCRATCH GOES BACK HERE#####
+score_dict = {}
+for i in range(len(grouped_match_list)):
+    score_team1 = grouped_match_list[i][1][0]
+    try:
+        score_dict[grouped_match_list[i][0]].append(int(score_dict[grouped_match_list[i][0]][-1]) + int(score_team1))
+    except:
+        score_dict[grouped_match_list[i][0]] = [int(score_team1)]
+
+    score_team2 = grouped_match_list[i][1][-1]
+    try:
+        score_dict[grouped_match_list[i][2]].append(int(score_dict[grouped_match_list[i][2]][-1]) + int(score_team2))
+    except:
+        score_dict[grouped_match_list[i][2]] = [int(score_team2)]
+
+#print(df1)
+
+max_key, max_value = max(score_dict.items(), key = lambda x: len(set(x[1])))
+max_match = copy.copy(max_value)
+for key in score_dict:
+    while len(score_dict[key]) < len(max_match):
+        score_dict[key].append(score_dict[key][-1])
+    if len(score_dict[key]) < len(max_match)+1:
+        score_dict[key].insert(0,0)
+
+
+print(score_dict)
+
+df = pd.DataFrame(columns=['Team Name', 'Score', 'Match Index'])
+for key in score_dict:
+    for i in range(len(max_value)):
+        df2 = pd.DataFrame([[key, score_dict[key][i], i]], columns=['Team Name', 'Score', 'Match Index'])
+        df = df.append(df2)
+
+print(df)
+import plotly.express as px
+
+fig = px.bar(df, x="Score", y="Team Name", color="Team Name",orientation='h',category_orders=df['Score'],
+  animation_frame="Match Index", animation_group="Team Name", range_x=[0,20])
+fig.show()
+
+
+
+
+
+
+
+import bar_chart_race as bcr
+
+df1 = pd.DataFrame.from_dict(score_dict)
+
+print(df1)
+bcr.bar_chart_race(df = df1, n_bars = 10,period_length=1200,
+                    filter_column_colors=True,
+                   sort='desc',title='2018 FIFA World Cup Goals by Country',
+                   filename = r'/Users/sulemanbazai/PycharmProjects/FIFA World Cup Data/test.mp4')
