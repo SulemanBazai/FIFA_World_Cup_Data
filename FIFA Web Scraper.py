008eeb416ce44791631d1f2f8721a3e27322c44c
sulemanbazai <sulemanbazai@gmail.com>
2021-06-02 01:10:14 -0500

    Update with using matplotlib to animate the bar graph over time successfully!

--- FIFA Web Scraper.py
+++ FIFA Web Scraper.py
@@ -10,7 +10,7 @@ import copy
 
 
 # Set URL, open and read the html page, and create the BeautifulSoup object.
-url = "https://www.fifa.com/worldcup/archive/russia2018/matches/"
+url = "https://www.fifa.com/worldcup/archive/russia2018/matches/"#"https://www.fifa.com/worldcup/archive/russia2018/matches/"
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 page = urlopen(req)
 html = page.read().decode("utf-8")
@@ -26,7 +26,7 @@ team_list = ['Australia','IR Iran','Japan','Saudi Arabia','Korea Republic','Egyp
 # Use the getText() function from the bs4 library to convert the BeautifulSoup object to a string.
 all_text = soup.getText()
 
-
+print(all_text)
 # Set up the game lists.
 
 # score_list is a list that will contain the score for the match.
@@ -71,6 +71,10 @@ for score_match in re.finditer("[0-9]-[0-9]", all_text):
         if all_text[e:e+40][s3:e3] in team_list:
             second_team_list.append(all_text[e:e+40][s3:e3])
 
+print(len(first_team_list),len(score_list),len(second_team_list))
+print(first_team_list)
+print(score_list)
+print(second_team_list)
 
 # Loop through the score_list, first_team_list and second_team_list to create a grouped match list.
 # This list will have the format [[first team match 1, score match 1, second team match 1],..[]..].
@@ -107,31 +111,66 @@ for key in score_dict:
 
 print(score_dict)
 
-df = pd.DataFrame(columns=['Team Name', 'Score', 'Match Index'])
+df = pd.DataFrame(columns=['Name', 'Score', 'Index'])
 for key in score_dict:
     for i in range(len(max_value)):
-        df2 = pd.DataFrame([[key, score_dict[key][i], i]], columns=['Team Name', 'Score', 'Match Index'])
+        df2 = pd.DataFrame([[key, score_dict[key][i], i]], columns=['Name', 'Score', 'Index'])
         df = df.append(df2)
 
 print(df)
-import plotly.express as px
 
-fig = px.bar(df, x="Score", y="Team Name", color="Team Name",orientation='h',category_orders=df['Score'],
-  animation_frame="Match Index", animation_group="Team Name", range_x=[0,20])
-fig.show()
-
-
-
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.animation as animation
+from IPython.display import HTML
+import random
 
 
+r_min=0
+r_max=255
+g_min=0
+g_max=255
+b_min=0
+b_max=255
+colors = dict()
+
+for key in team_list:
+    red = random.randint(r_min, r_max)
+    green = random.randint(g_min, g_max)
+    blue = random.randint(b_min, b_max)
+    rgb_string = (red,green,blue)
+    colors[key] = '#%02x%02x%02x' % rgb_string
 
 
-import bar_chart_race as bcr
 
-df1 = pd.DataFrame.from_dict(score_dict)
+fig, ax = plt.subplots(figsize=(15, 8))
 
-print(df1)
-bcr.bar_chart_race(df = df1, n_bars = 10,period_length=1200,
-                    filter_column_colors=True,
-                   sort='desc',title='2018 FIFA World Cup Goals by Country',
-                   filename = r'/Users/sulemanbazai/PycharmProjects/FIFA World Cup Data/test.mp4')
+
+def draw_barchart(year):
+    dff = df[df['Index'].eq(year)].sort_values(by='Score', ascending=True).tail(10)
+    ax.clear()
+    ax.barh(dff['Name'], dff['Score'], color=[colors[x] for x in dff['Name']])
+    #dx = dff['Score'].max() / 2000
+    for i, (value, name) in enumerate(zip(dff['Score'], dff['Name'])):
+        ax.text(value , i,     name,           size=14, weight=600, ha='right', va='center')
+        #ax.text(value , i- .25, name, size=10, color='#444444', ha='right', va='baseline')
+        ax.text(value , i, f'{value:,.0f}', size=14, ha='left', va='center')
+    # ... polished styles
+    ax.text(1, 0.4, year, transform=ax.transAxes, color='#777777', size=46, ha='right', weight=800)
+    ax.text(0, 1.06, 'Goals', transform=ax.transAxes, size=12, color='#777777')
+    ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+    ax.xaxis.set_ticks_position('top')
+    ax.tick_params(axis='x', colors='#777777', labelsize=12)
+    ax.set_yticks([])
+    ax.margins(0, 0.01)
+    ax.grid(which='major', axis='x', linestyle='-')
+    ax.set_axisbelow(True)
+    ax.text(0, 1.12, '2018 FIFA World Cup Goals by Country',
+            transform=ax.transAxes, size=18, weight=600, ha='left')
+    ax.text(1, 0, 'by Suleman Bazai', transform=ax.transAxes, ha='right',
+            color='#777777', bbox=dict(facecolor='white', alpha=0.8, edgecolor='white'))
+    plt.box(False)
+
+
+animator = animation.FuncAnimation(fig, draw_barchart, frames=range(0,len(max_match)+1),interval=600,repeat=False)
+plt.show()
